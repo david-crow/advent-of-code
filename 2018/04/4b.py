404a2b7fd09c6a9b3df2403a1f6942cf8afa7271
David Crow <davidrheacrow@gmail.com>
2022-12-05 15:32:50 -0800

    2018.4; 2022.5

---
+++ 2018/04/4b.py
@@ -0,0 +1,32 @@
+# API stuff
+
+from aocd import get_data, submit
+part = "a" if "a" in __file__.split("/")[-1] else "b"
+day, year = int(__file__.split("/")[-2]), int(__file__.split("/")[-3])
+data = get_data(day=day, year=year)
+
+# solution
+
+import re
+from collections import defaultdict
+
+data = [d.split("] ") for d in sorted(data.split("\n"))]
+data = [(list(map(int, re.findall("[0-9]+", d[0]))), d[1].split(" ")) for d in data]
+times = defaultdict(lambda: [0] * 60)
+
+for d in data:
+    match(event := d[1][0]):
+        case "Guard":
+            guard = d[1][1][1:]
+        case "falls":
+            sleep_time = d[0][4]
+        case "wakes":
+            for t in range(sleep_time, d[0][4]):
+                times[guard][t] += 1
+
+guard = max(times, key=lambda k: max(times[k]))
+answer = int(guard) * times[guard].index(max(times[guard]))
+
+# submission
+
+submit(answer, part=part, day=day, year=year)
---
+++ 2022/05/5a.py
@@ -0,0 +1,33 @@
+# API stuff
+
+from aocd import get_data, submit
+part = "a" if "a" in __file__.split("/")[-1] else "b"
+day, year = int(__file__.split("/")[-2]), int(__file__.split("/")[-3])
+data = get_data(day=day, year=year)
+
+# solution
+
+from collections import defaultdict
+import re
+
+data = [d.split("\n") for d in data.split("\n\n")]
+data[1] = [list(map(int, re.findall("[0-9]+", d))) for d in data[1]]
+stacks = defaultdict(list)
+
+# generalizable to any set of stacks
+# rows must use four chars between stacks, e.g., [A]  [B]  [ ]  [C]
+for d in data[0][:-1]:
+    for i, letter in enumerate(d[1::4]):
+        if letter != " ":
+            stacks[i + 1].append(letter)
+
+# make the moves
+for move in data[1]:
+    stacks[move[2]] = stacks[move[1]][:move[0]][::-1] + stacks[move[2]]
+    stacks[move[1]] = stacks[move[1]][move[0]:]
+
+answer = "".join([v[0] for _, v in sorted(stacks.items())])
+
+# submission
+
+submit(answer, part=part, day=day, year=year)
