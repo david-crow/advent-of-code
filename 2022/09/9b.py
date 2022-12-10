5ddfbe56c181c0697f4a437294615ac237d701b7
David Crow <davidrheacrow@gmail.com>
2022-12-09 16:32:00 -0800

    2022.9

---
+++ 2022/09/9b.py
@@ -0,0 +1,42 @@
+# API stuff
+
+from aocd import get_data, submit, lines, numbers
+part = "a" if "a" in __file__.split("/")[-1] else "b"
+day, year = int(__file__.split("/")[-2]), int(__file__.split("/")[-3])
+data = get_data(day=day, year=year)
+
+# solution
+
+def updateHead():
+    hx = hy = 0
+
+    for line in lines:
+        direction, distance = line.split()
+
+        for _ in range(int(distance)):
+            hx += (direction == "R") - (direction == "L")
+            hy += (direction == "U") - (direction == "D")
+
+            yield hx, hy
+
+def updateTail(head):
+    tx = ty = 0
+
+    for hx, hy in head:
+        if abs(hx - tx) > 1 or abs(hy - ty) > 1:
+            ty += (hy > ty) - (hy < ty)
+            tx += (hx > tx) - (hx < tx)
+
+        yield tx, ty
+
+knot = list(updateHead())
+
+for _ in range(9):
+    knot = updateTail(knot)
+
+answer = len(set(knot))
+
+# submission
+
+print(answer)
+# submit(answer, part=part, day=day, year=year)
